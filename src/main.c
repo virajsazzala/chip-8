942bcb1252db7875c671eace453a490dff88b7bf
SNG Viraj Reddy <vir200319@gmail.com>
2026-08-16 15:49:08 +0530

    feat: add decode_execute base & opcodes for IBM
    Add decode_execute() as the switch func for
    opcodes. Implement 1NNN, 6XNN, 7XNN, ANNN, and
    DXYN instructions required for IBM logo test.
    Add helper functions to draw display to terminal
    console.
    
    Signed-off-by: SNG Viraj Reddy <vir200319@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include <assert.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
@@ -68,16 +68,129 @@ uint16_t fetch() {
 	return ins;
 }
 
+void clear_screen() { memset(display, 0, (DIS_H * DIS_W)); }
+
+void draw(uint8_t rx, uint8_t ry, uint8_t n) {
+	registers[0xF] = 0;
+	for (int row = 0; row < n; row++) {
+		uint8_t nbyte = memory[i + row];
+		for (int col = 0; col < 8; col++) {
+			uint8_t pixel = (nbyte >> (7 - col)) & 1;
+			if (pixel) {
+				int px = (rx + col) % DIS_W;
+				int py = (ry + row) % DIS_H;
+				int idx = py * DIS_W + px;
+				if (display[idx]) {
+					registers[0xF] = 1;
+				}
+				display[idx] ^= 1;
+			}
+		}
+	}
+}
+
+void print_display() {
+	printf("\033[H\033[2J"); // move cursor to top, clear screen
+	for (int y = 0; y < DIS_H; y++) {
+		for (int x = 0; x < DIS_W; x++) {
+			printf(display[y * DIS_W + x] ? "#" : ".");
+		}
+		printf("\n");
+	}
+}
+
+void decode_execute(uint16_t ins) {
+	uint8_t op = (ins >> 12) & 0xF;
+	uint8_t x = (ins >> 8) & 0xF;
+	uint8_t y = (ins >> 4) & 0xF;
+	uint8_t n = ins & 0xF;
+	uint8_t nn = ins & 0xFF;
+	uint16_t nnn = ins & 0xFFF;
+
+	switch (op) {
+	case 0x0:
+		if (ins == 0x00E0) {
+			clear_screen();
+		}
+		break;
+
+	case 0x1:
+		// 1NNN - Jump to location nnn
+		pc = nnn;
+		break;
+
+	case 0x2:
+		break;
+
+	case 0x3:
+		break;
+
+	case 0x4:
+		break;
+
+	case 0x5:
+		break;
+
+	case 0x6:
+		// 6XNN: Set VX = NN
+		registers[x] = nn;
+		break;
+
+	case 0x7:
+		// 7XNN: Set VX = VX + NN
+		registers[x] += nn;
+		break;
+
+	case 0x8:
+		break;
+
+	case 0x9:
+		break;
+
+	case 0xA:
+		// ANNN: Set I = NNN
+		i = nnn;
+		break;
+
+	case 0xB:
+		break;
+
+	case 0xC:
+		break;
+
+	case 0xD:
+		// DXYN: Display n-byte sprite starting at memory I
+		// at (VX, VY), set VF = collision
+		draw(registers[x], registers[y], n);
+		break;
+
+	case 0xE:
+		break;
+
+	case 0xF:
+		break;
+	}
+}
+
 int main(int argc, char **argv) {
 	assert(argc == 2);
 	init(argv[1]);
-	
+
 	bool running = true;
 	while (running) {
+		if (pc >= MEM_CAPACITY) {
+			// temporary to avoid inf loop
+			running = false;
+			break;
+		}
+
 		uint16_t opcode = fetch();
 		printf("%04X\n", opcode);
+
+		decode_execute(opcode);
+		print_display();
 	}
-	
+
 	printf("hello, world!\n");
 	return 0;
 }
